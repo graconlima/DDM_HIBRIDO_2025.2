2b9fc3fd67b6ff5e0973e891406e0c890556b8cc
1958314 <graconlima@yahoo.com.br>
2025-10-29 20:51:06 -0300

    Do Lab 13: Layout Column e Row.

--- lib/paginas/2) exemplos_widgets/layout.dart
+++ lib/paginas/2) exemplos_widgets/layout.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 //Exemplo 1: Layout para aplicativo não Material
-/*void main(){
+void main(){
   runApp(MinhaApp());
 }
 
@@ -9,10 +9,35 @@ class MinhaApp extends StatelessWidget{
   Widget build(BuildContext bc){
     return Center(child: Text("Ola Mundo", textDirection: TextDirection.ltr));
   }
+}
+
+//Exemplo 2: Container (parametros)
+/*void main(){
+  runApp(MinhaApp());
+}
+
+class MinhaApp extends StatelessWidget{
+  Widget build(BuildContext bc){
+    return Container(
+      width: 50,
+      height: 50,
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.amber,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Text(
+        'Caixa decorada',
+        style: TextStyle(color: Colors.white),
+        textDirection: TextDirection.rtl,
+      ),
+    );
+  }
 }*/
 
-//Exemplo 2: Layout para aplicativo Material
-void main(){
+//Exemplo 3: Layout para aplicativo Material
+/*void main(){
   runApp(MinhaApp());
 }
 
@@ -26,4 +51,40 @@ class MinhaApp extends StatelessWidget{
         )
     );
   }
+}*/
+
+//Exemplo 4: Row
+/*void main(){
+  runApp(MinhaApp());
 }
+
+class MinhaApp extends StatelessWidget{
+  Widget build(BuildContext bc){
+    return MaterialApp(
+        title: "Ola Mundo!",
+        home: Scaffold(
+            appBar: AppBar(title: Text("OlaApp")),
+            body:
+            // Column(
+            //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //     crossAxisAlignment: CrossAxisAlignment.stretch,
+            //     mainAxisSize: MainAxisSize.min,
+            //     children: [
+            //       Icon(Icons.star, color: Colors.amber),
+            //       Icon(Icons.star, color: Colors.amber),
+            //       Icon(Icons.star, color: Colors.amber),
+            //     ])
+
+            // Row(
+            //     mainAxisAlignment: MainAxisAlignment.spaceAround,
+            //     crossAxisAlignment: CrossAxisAlignment.stretch,
+            //     mainAxisSize: MainAxisSize.max,
+            //     children: [
+            //       Icon(Icons.star, color: Colors.amber),
+            //       Icon(Icons.star, color: Colors.amber),
+            //       Icon(Icons.star, color: Colors.amber),
+            //     ])
+        )
+    );
+  }
+}*/
