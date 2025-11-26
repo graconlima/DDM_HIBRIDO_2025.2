480221baf3a6c055bf4c94a5ed77655e4f47e131
1958314 <graconlima@yahoo.com.br>
2025-11-26 20:47:49 -0300

    Lab 13: Navegacao e animacao de transicao

--- lib/paginas/3) exemplos_tratamento_eventos/3)gestos.dart
+++ lib/paginas/3) exemplos_tratamento_eventos/3)gestos.dart
@@ -1,4 +1,4 @@
-//import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
 //Exemplo 1: clique simples
 /*main(){
--- lib/paginas/4) exemplos_navegacao/1)exemplos_navegacao.dart
+++ lib/paginas/4) exemplos_navegacao/1)exemplos_navegacao.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-void main(){
+//Exemplo: transicao do Material Design
+/*void main(){
   runApp(MaterialApp(home: TelaInicial()));
 }
 class TelaInicial extends StatelessWidget{
@@ -9,6 +10,49 @@ class TelaInicial extends StatelessWidget{
         body: Row(children: <Widget>[
           FloatingActionButton(heroTag: "bt1", onPressed: (){Navigator.of(bc).push(MaterialPageRoute(builder: (bc) => PrimeiraTela()));}, child: Text("Tela 1")),
           FloatingActionButton(heroTag: "bt2", onPressed: (){Navigator.of(bc).push(MaterialPageRoute(builder: (bc) => SegundaTela()));}, child: Text("Tela 2"))
+        ])
+    );
+  }
+}
+class PrimeiraTela extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: FloatingActionButton(child: Text("Voltar"), onPressed: (){Navigator.pop(context);})));
+  }
+}
+class SegundaTela extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: FloatingActionButton(child: Text("Voltar"), onPressed: (){Navigator.pop(context);})));
+  }
+}*/
+
+//Exemplo 2: animacao de transicao
+import 'package:page_transition/page_transition.dart';
+void main(){
+  runApp(MaterialApp(home: TelaInicial()));
+}
+class TelaInicial extends StatelessWidget{
+  Widget build(BuildContext bc){
+    return Scaffold(
+        appBar: AppBar(title: Text("Ola")),
+        body: Row(children: <Widget>[
+          FloatingActionButton(
+              heroTag: "bt1",
+              onPressed: (){Navigator.of(bc).push(
+                PageTransition(
+                    child: PrimeiraTela(),
+                    type: PageTransitionType.fade,
+                    duration: Duration(milliseconds: 600),
+                    reverseDuration: Duration(milliseconds: 300)
+                )
+              );}, child: Text("Tela 1")),
+          FloatingActionButton(
+              heroTag: "bt2",
+              onPressed: (){Navigator.of(bc).push(
+                  MaterialPageRoute(
+                      builder: (bc) => SegundaTela())
+              );}, child: Text("Tela 2"))
         ])
     );
   }
